563a7243d1ffda5da81052763ab4f739f48136de
David M <mullaned@gmail.com>
2018-09-25 20:03:19 +0100

    adding custom component to form

--- src/App.vue
+++ src/App.vue
@@ -95,6 +95,11 @@
                     </select>
                 </div>
             </div>
+             <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-switch v-model="dataSwitch"></app-switch>
+                </div>
+            </div>
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
@@ -122,7 +127,7 @@
                         </ul>
                         <p>Gender: {{gender}}</p>
                         <p>Priority: {{selectedPriority}}</p>
-                        <p>Switched:</p>
+                        <p>Switched: {{dataSwitch}}</p>
                     </div>
                 </div>
             </div>
@@ -131,6 +136,8 @@
 </template>
 
 <script>
+    import Switch from './Switch.vue'
+
     export default {
         data() {
             return {
@@ -143,10 +150,14 @@
                 sendMail:[],
                 gender: 'Male',
                 priorities:['High','Medium','Low'],
-                selectedPriority:'High'
+                selectedPriority:'High',
+                dataSwitch: true
                 
 
             }
+        },
+        components: {
+            appSwitch: Switch
         }
     }
 </script>
